c35ae0fbecedd95e2d5c26ff077e96348636baa1
Dcwahl <diegowahl@gmail.com>
2025-11-10 19:29:26 -0500

    notification stuff, probably needs some cleanup but whatever

--- src/db.rs
+++ src/db.rs
@@ -7,14 +7,27 @@ pub async fn test_init() -> Result<SqlitePool, sqlx::Error>{
     let pool = SqlitePool::connect("sqlite://notifications.db?mode=rwc").await?;
 
 
-    let test_create_table = r#"
-        CREATE TABLE IF NOT EXISTS test_table (
+    // let test_create_table = r#"
+    //     CREATE TABLE IF NOT EXISTS test_table (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         name TEXT NOT NULL
+    //     );
+    // "#;
+
+    // sqlx::query(test_create_table).execute(&pool).await?;
+
+
+    let create_notifications_table = r#"
+        CREATE TABLE IF NOT EXISTS notifications (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
+            title TEXT NOT NULL,
+            message TEXT NOT NULL,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         );
     "#;
 
-    sqlx::query(test_create_table).execute(&pool).await?;
+    sqlx::query(create_notifications_table).execute(&pool).await?;
+
 
     Ok(pool)
 }
@@ -30,4 +43,33 @@ pub async fn test_write_to_db(pool: &SqlitePool, name: &str) -> Result<(), sqlx:
         .await?;
 
     Ok(())
+}
+
+pub async fn add_notification(pool: &SqlitePool, title: &str, message: &str) -> Result<(i64, String, String, String), sqlx::Error> {
+    let insert_query = r#"
+        INSERT INTO notifications (title, message)
+        VALUES (?1, ?2)
+        RETURNING id, title, message, timestamp;
+    "#;
+
+    let row = sqlx::query_as::<_, (i64, String, String, String)>(insert_query)
+        .bind(title)
+        .bind(message)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(row)
+}
+
+pub async fn get_latest_notifications(pool: &SqlitePool, id: &i64) -> Result<Vec<(i64, String, String, String)>, sqlx::Error> {
+    let select_query = r#"
+        SELECT id, title, message, timestamp FROM notifications WHERE id > ?1 ORDER BY id ASC;
+    "#;
+
+    let rows = sqlx::query_as::<_, (i64, String, String, String)>(select_query)
+        .bind(id)
+        .fetch_all(pool)
+        .await?;
+
+    Ok(rows)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 use axum::{
-    routing::get,
     Router,
-    extract::State,
+    extract::{Query, State},
+    routing::{get, post},
+    Json
 };
-
+use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
-
 
 mod db;
 
 #[tokio::main]
 async fn main() {
-
     //init db
     let pool = db::test_init().await.unwrap();
-
 
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/test_insert", get(test_insert_handler))
+        .route("/notifications", get(fetch_notifications).post(create_notification))
         .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
@@ -28,7 +27,56 @@ async fn main() {
 }
 
 async fn test_insert_handler(State(pool): State<SqlitePool>) -> &'static str {
-
     db::test_write_to_db(&pool, "Test Name").await.unwrap();
     "Inserted test name into database"
+}
+
+#[derive(Deserialize)]
+struct NotificationQuery {
+    id: Option<i64>, // Option because it might not be provided
+}
+
+//the #derive thing lets us convert to json easily
+#[derive(Serialize)]
+struct Notification {
+    id: i64,
+    title: String,
+    message: String,
+    timestamp: String,
+}
+
+#[derive(Deserialize)]
+struct CreateNotificationRequest {
+    title: String,
+    message: String,
+}
+
+async fn create_notification(pool: State<SqlitePool>, notification: Json<CreateNotificationRequest>) -> Json<Notification> {
+    let title = &notification.title;
+    let message = &notification.message;
+    let row = db::add_notification(&pool, title, message).await.unwrap();
+
+    let (id, title, message, timestamp) = row;
+
+    Json(Notification { id, title, message, timestamp })
+}
+
+async fn fetch_notifications(
+    State(pool): State<SqlitePool>,
+    Query(params): Query<NotificationQuery>,
+) -> Json<Vec<Notification>> {
+    let last_seen_id = params.id.unwrap_or(-1);
+
+    let rows = db::get_latest_notifications(&pool, &last_seen_id)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("DB error: {}", e);
+            vec![]  // return empty vec on error
+        });
+    
+    let notifications: Vec<Notification> = rows.into_iter()
+        .map(|(id, title, message, timestamp)| Notification { id, title, message, timestamp })
+        .collect();
+
+    Json(notifications)
 }
